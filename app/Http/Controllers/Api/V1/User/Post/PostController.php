c6082802de5e74f7ed98731a6b0f5a8e3441bfc0
Confidence Ugolo <ugoloconfidence@gmail.com>
2024-07-16 12:31:54 +0100

    Minor fix

--- app/Http/Controllers/Api/V1/User/Post/PostController.php
+++ app/Http/Controllers/Api/V1/User/Post/PostController.php
@@ -30,7 +30,7 @@ class PostController extends Controller
     public function index(Request $request)
     {
         try {
-            $posts = $this->post_service->list($request->all())->status()->unblocked()->inRandomOrder()->paginate(AppConstants::API_PAGINATION_SIZE)->appends($request->query());
+            $posts = $this->post_service->list($request->all())->status()->unblocked()->paginate(AppConstants::API_PAGINATION_SIZE)->appends($request->query());
             $data = collectPagination($posts);
             $data["data"] = PostResource::collection($data["data"]);
             return ApiHelper::validResponse("Posts returned successfully", $data);
