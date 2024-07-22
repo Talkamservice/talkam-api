df058192224e8556fdee2c0b39d2d289deca85ed
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-07-22 17:38:20 +0100

    adding additional avatar to to existing avatars

--- database/seeders/AvatarTableSeeder.php
+++ database/seeders/AvatarTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Constants\General\StatusConstants;
+use App\Models\Avatar;
 use App\Services\User\AvatarService;
 use Illuminate\Database\Seeder;
 use Illuminate\Http\Testing\FileFactory;
@@ -18,21 +19,50 @@ class AvatarTableSeeder extends Seeder
      */
     public function run()
     {
-        $random_files = File::files(public_path("samples/avatars"));
-        foreach ($random_files as $key => $random_file) {
+
+        // Get all avatar files from both directories
+        $avatar_files = array_merge(
+            File::files(public_path("samples/avatars")),
+            File::files(public_path("samples/additional_avatar"))
+        );
+
+        foreach ($avatar_files as $key => $avatar_file) {
             try {
-                $avatar_file = $random_file ?? fake()->randomElement($random_files);
+                $avatar_name = "Avatar " . $key;
+                // Check if the avatar already exists
+                if (Avatar::where('name', $avatar_name)->exists()) {
+                    // Skip the avatar if it already exists
+                    continue;
+                }
+                // Create the avatar
                 (new AvatarService)->create([
-                    "name" => "Avatar " . $key,
+                    "name" => $avatar_name,
                     "description" => "Avatar " . $key,
                     "image" => $this->createImage($avatar_file->getFilename(), $avatar_file->getPathname()),
                     "status" => StatusConstants::ACTIVE
                 ]);
             } catch (\Throwable $th) {
+                // Log the exception or handle it as needed
                 throw $th;
             }
-
         }
+
+        // $random_files = File::files(public_path("samples/avatars"));
+        // $random_files = File::files(public_path("samples/profile-avatar"));
+        // foreach ($random_files as $key => $random_file) {
+        //     try {
+        //         $avatar_file = $random_file ?? fake()->randomElement($random_files);
+        //         (new AvatarService)->create([
+        //             "name" => "Avatar " . $key,
+        //             "description" => "Avatar " . $key,
+        //             "image" => $this->createImage($avatar_file->getFilename(), $avatar_file->getPathname()),
+        //             "status" => StatusConstants::ACTIVE
+        //         ]);
+        //     } catch (\Throwable $th) {
+        //         throw $th;
+        //     }
+
+        // }
     }
 
     function createImage($name, $path)
