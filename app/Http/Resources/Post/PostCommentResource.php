6b29501fe0eabe908255c1147ddda1faee3968f1
Confidence Ugolo <ugoloconfidence@gmail.com>
2024-07-25 10:00:49 +0100

    Added post data to comments endpoint

--- app/Http/Resources/Post/PostCommentResource.php
+++ app/Http/Resources/Post/PostCommentResource.php
@@ -25,7 +25,7 @@ class PostCommentResource extends JsonResource
 
         return [
             "id" => $this->id,
-            "post_id" => $this->post_id,
+            "post" => PostResource::custom($this->post),
             "user" => !empty($this->user) ? UserResource::custom($this->user) : null,
             "comment" => $this->comment,
             "is_anonymous" => $this->is_anonymous,
