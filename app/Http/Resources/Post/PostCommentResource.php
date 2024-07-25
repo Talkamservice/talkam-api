66f96b6b7abaaf82b75ae230a82e75313addf633
Confidence Ugolo <ugoloconfidence@gmail.com>
2024-07-25 09:52:09 +0100

    Minor fix

--- app/Http/Resources/Post/PostCommentResource.php
+++ app/Http/Resources/Post/PostCommentResource.php
@@ -25,6 +25,7 @@ class PostCommentResource extends JsonResource
 
         return [
             "id" => $this->id,
+            "post_id" => $this->post_id,
             "user" => !empty($this->user) ? UserResource::custom($this->user) : null,
             "comment" => $this->comment,
             "is_anonymous" => $this->is_anonymous,
