a7c260e5821304c771312b359c75977e6035fdca
Joel Omojefe <joelomojefe@gmail.com>
2024-07-29 13:35:49 +0100

    Guest endpoint done

--- app/Http/Resources/Post/PostResource.php
+++ app/Http/Resources/Post/PostResource.php
@@ -20,7 +20,7 @@ class PostResource extends JsonResource
 
     public function toArray($request)
     {
-        $user_reaction = auth("sanctum")->check() ? UserPostReaction::where(["post_id" => $this->id, "user_id" => auth()->id()])->first() : null;
+        $user_reaction = UserPostReaction::where(["post_id" => $this->id, "user_id" => auth()->id()])->first();
         $likes = UserPostReaction::where(["post_id" => $this->id, "action" => PostConstants::LIKE])->count();
 
         return [
