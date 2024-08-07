00168ad9a0d6d165839070ddf1a85e2a3a7730ab
Joel-prodevs <joel@prodevs.io>
2024-08-07 16:56:37 +0100

    Minor fix

--- app/Http/Resources/Group/GroupMemberResource.php
+++ app/Http/Resources/Group/GroupMemberResource.php
@@ -22,7 +22,8 @@ class GroupMemberResource extends JsonResource
             "status" => $this->status,
             "group" => GroupResource::custom($this->group),
             "user" => UserResource::custom($this->user),
-            "created_at" => $this->created_at,
+            "created_at" => formatDate($this->created_at),
+            "updated_at" => formatDate($this->updated_at)
         ];
     }
 }
