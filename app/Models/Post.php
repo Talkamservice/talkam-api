0980c266f3d0dee200cf0e87aaea20a31f988d34
Joel-prodevs <joel@prodevs.io>
2024-08-07 14:51:28 +0100

    Minor fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -76,7 +76,7 @@ class Post extends Model
     public function scopeUnblocked($query)
     {
         if (auth("sanctum")->check()) {
-            $query->whereDoesntHave('user.blockedUsers', function ($q) {
+            $query->whereDoesntHave('user', function ($q) {
                 $q->whereDoesntHave("blockedUsers", function ($blocked_user) {
                     $blocked_user->where("blocked_user_id", auth("sanctum")->id());
                 });
