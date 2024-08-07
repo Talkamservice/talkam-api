b1f51cbc7bc863d50dd6b10cf2a43ebcf82ba344
Joel-prodevs <joel@prodevs.io>
2024-08-07 11:57:42 +0100

    Minor fix

--- app/Http/Controllers/Api/V1/User/Post/SearchController.php
+++ app/Http/Controllers/Api/V1/User/Post/SearchController.php
@@ -29,7 +29,7 @@ class SearchController extends Controller
     {
         try {
             $response = $this->search_service->search($request->all());
-            $records = $response["records"]->paginate(AppConstants::API_PAGINATION_SIZE);
+            $records = $response["records"]->paginate(AppConstants::API_PAGINATION_SIZE)->appends($request->query());
 
             $data = collectPagination($records);
 
