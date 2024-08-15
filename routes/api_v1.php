572606407bb2ed6b0c14ee0dda0f7ec57a236bbd
Joel-prodevs <joel@prodevs.io>
2024-08-15 14:56:51 +0100

    Current conversation

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -146,6 +146,7 @@ Route::middleware(["auth:sanctum"])->group(function () {
             Route::prefix("conversations")->as("conversations.")->group(function () {
                 Route::post("update-status", [ConversationController::class, "updateStatus"])->name("update-status");
                 Route::post("report", [ConversationController::class, "report"])->name("report");
+            Route::post("/current-conversation", [ConversationController::class, "currentConversation"])->name("current-conversation");
             });
         });
 
