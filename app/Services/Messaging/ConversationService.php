e5de2c5771f90ce86d17365cf1ee182f1922c17d
Joel-prodevs <joel@prodevs.io>
2024-08-19 17:02:07 +0100

    Minor fix

--- app/Services/Messaging/ConversationService.php
+++ app/Services/Messaging/ConversationService.php
@@ -200,10 +200,6 @@ class ConversationService
                 $query->where('sender_id', $data["sender_id"])
                     ->whereNot("status", StatusConstants::AWAITING_APPROVAL);
             });
-            // ->orWhere(function ($q) use ($data) {
-            //     $q->where('receiver_id', $data["sender_id"])
-            //         ->whereNot("status", StatusConstants::AWAITING_APPROVAL);
-            // });
         }
 
         if (!empty($key = $data["status"] ?? null)) {
