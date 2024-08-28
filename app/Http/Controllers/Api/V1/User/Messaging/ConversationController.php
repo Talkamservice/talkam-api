e56cbe245a54efcedb102c5c1de6ebb6589a89e5
Joel-prodevs <joel@prodevs.io>
2024-08-28 16:35:10 +0100

    Minor fix

--- app/Http/Controllers/Api/V1/User/Messaging/ConversationController.php
+++ app/Http/Controllers/Api/V1/User/Messaging/ConversationController.php
@@ -41,7 +41,7 @@ class ConversationController extends Controller
             $conversation = $this->conversation_service->show($id);
             $data = ConversationResource::make($conversation);
             $other_member = $conversation->members()->whereNot("user_id", auth()->id())->first();
-            broadcast(new RefreshNotification($other_member->id))->toOthers();
+            broadcast(new RefreshNotification($other_member->user_id))->toOthers();
             return ApiHelper::validResponse("Conversation details returned successfully", $data);
         } catch (ModelNotFoundException $th) {
             return ApiHelper::problemResponse($th->getMessage(), ApiConstants::BAD_REQ_ERR_CODE, null, $th);
