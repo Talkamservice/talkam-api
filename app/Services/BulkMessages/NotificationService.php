8f7904fb4642932434f7018d5227e7a1fa8d44a4
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-08-30 14:40:26 +0100

    saving state

--- app/Services/BulkMessages/NotificationService.php
+++ app/Services/BulkMessages/NotificationService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services\bulkMessages;
+namespace App\Services\BulkMessages;
 
 use App\Constants\General\AppConstants;
 use App\Constants\General\StatusConstants;
@@ -30,6 +30,11 @@ class NotificationService
 
     public static function validate(array $data, $id = null)
     {
+
+        // If the type is Broadcast, get all user IDs
+        if ($data['type'] === 'Broadcast') {
+            $data['user_id'] = User::pluck('id')->toArray(); // Retrieve all user IDs
+        }
         $validator = Validator::make($data, [
             'title' => 'required|string|max:255',
             'message' => 'required|string',
@@ -122,6 +127,5 @@ class NotificationService
         $notification->update([
             "status" => $status
         ]);
-
     }
 }
