35fe053e95121dea45dea0b93205f372b773167c
Joel-prodevs <joel@prodevs.io>
2024-08-30 16:29:30 +0100

    Minor fix

--- app/Http/Controllers/Api/V1/User/Post/PostController.php
+++ app/Http/Controllers/Api/V1/User/Post/PostController.php
@@ -145,8 +145,8 @@ class PostController extends Controller
                 if (!empty($request->exclude_anonymous)) {
                     $post->where("is_anonymous", 0);
                 }
-            })->paginate(AppConstants::API_PAGINATION_SIZE)
-                ->latest()->appends($request->query());
+            })->latest()->paginate(AppConstants::API_PAGINATION_SIZE)
+                ->appends($request->query());
 
             $data = collectPagination($attachments);
             $data["data"] = PostAttachmentResource::collection($data["data"]);
