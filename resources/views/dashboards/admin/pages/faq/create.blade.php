deae88254db4a54febe32d4e4e5232659a85a828
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-09-06 11:53:30 +0100

    faq updated

--- resources/views/dashboards/admin/pages/faq/create.blade.php
+++ resources/views/dashboards/admin/pages/faq/create.blade.php
@@ -26,6 +26,21 @@
                             @isset($faq)
                                 @method('patch')
                             @endisset
+                            <div class="row col-xl-9 col-sm-12 mb-3">
+                                <label for="input-placeholder"
+                                    class="form-label col-xl-2 col-lg-2 col-md-2 col-sm-2">Category (Optional)</label>
+                                <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                                    <select name="faq_category_id" id="" class="form-control">
+                                        <option value="" disabled selected>Select Option</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}"
+                                                {{ (old('faq_category_id') ?? ($faq->faq_category_id ?? '')) == $category->id ? 'selected' : '' }}>
+                                                {{ $category->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
                             <div class="gy-4 mb-4">
                                 <div class="row col-xl-9 col-sm-12 mb-3">
                                     <label for="input-placeholder"
@@ -34,21 +49,6 @@
                                         <input type="text" class="form-control" name="question" id="input-placeholder"
                                             value="{{ old('question') ?? ($faq->question ?? '') }}"
                                             placeholder="Enter question">
-                                    </div>
-                                </div>
-                                <div class="row col-xl-9 col-sm-12 mb-3">
-                                    <label for="input-placeholder"
-                                        class="form-label col-xl-2 col-lg-2 col-md-2 col-sm-2">Category</label>
-                                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
-                                        <select name="faq_category_id" id="" class="form-control">
-                                            <option value="" disabled selected>Select Option</option>
-                                            @foreach ($categories as $category)
-                                                <option value="{{ $category->id }}"
-                                                    {{ (old('faq_category_id') ?? ($faq->faq_category_id ?? '')) == $category->id ? 'selected' : '' }}>
-                                                    {{ $category->name }}
-                                                </option>
-                                            @endforeach
-                                        </select>
                                     </div>
                                 </div>
                                 <div class="row col-xl-9 col-sm-12 mb-3">
