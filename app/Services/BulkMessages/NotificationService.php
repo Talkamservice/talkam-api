509904adf05b998660e1115679f348dc83e184af
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-09-06 10:27:51 +0100

    saving state

--- app/Services/BulkMessages/NotificationService.php
+++ app/Services/BulkMessages/NotificationService.php
@@ -4,7 +4,6 @@ namespace App\Services\BulkMessages;
 
 use App\Constants\ActivityLog\ActivitiesConstants;
 use App\Constants\ActivityLog\ActivityLogConstants;
-use App\Constants\General\AppConstants;
 use App\Constants\General\StatusConstants;
 use App\Exceptions\General\InvalidRequestException;
 use App\Exceptions\General\ModelNotFoundException;
@@ -14,9 +13,8 @@ use App\Models\SendBulkNotification;
 use App\Models\User;
 use App\Services\ActivityLog\ActivityLogService;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Bus\Batch;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
@@ -33,11 +31,10 @@ class NotificationService
 
     public static function validate(array $data, $id = null)
     {
-
-        // If the type is Broadcast, get all user IDs
         if ($data['type'] === 'Broadcast') {
-            $data['user_id'] = User::pluck('id')->toArray(); // Retrieve all user IDs
+            $data['user_id'] = User::pluck('id')->toArray();
         }
+
         $validator = Validator::make($data, [
             'title' => 'required|string|max:255',
             'message' => 'required|string',
@@ -45,7 +42,7 @@ class NotificationService
             'status' => 'nullable|string',
             'schedule_date' => 'nullable|date',
             'user_id' => [
-                'required_if:type,Single', // required for 'single' and 'multiple'
+                'required_if:type,Single',
                 'exists:users,id',
             ],
         ]);
@@ -57,15 +54,16 @@ class NotificationService
         return $validator->validated();
     }
 
-
     public function send(array $data, $notificationId = null)
     {
-        $data = $this->validate($data);
-        // Handle existing or new notification
+        $data = self::validate($data);
+
         if ($notificationId) {
             $notification = self::getById($notificationId);
-            $old_notification = $notification;
+            $old_notification = $notification->toArray();
+
             $notification->update($data);
+
             (new ActivityLogService)
                 ->setEvent("updated")
                 ->setTitle("Bulk Notification Updated")
@@ -74,10 +72,10 @@ class NotificationService
                 ->setActivity(ActivitiesConstants::UPDATED_USER_NOTIFICATION)
                 ->setModel(SendBulkNotification::class, $notification->id)
                 ->setAdmin(auth()->user()->id)
-                ->setData(
-                    ["Old Bulk Notification" =>  $old_notification->toArray()],
-                    ["Bulk Notification" =>  $notification->refresh()->toArray()],
-                )
+                ->setData([
+                    "Old Bulk Notification" => $old_notification,
+                    "Updated Bulk Notification" => $notification->refresh()->toArray(),
+                ])
                 ->setUrl(request()->fullUrl())
                 ->log();
         } else {
@@ -90,9 +88,9 @@ class NotificationService
                 ->setActivity(ActivitiesConstants::CREATED_USER_NOTIFICATION)
                 ->setModel(SendBulkNotification::class, $notification->id)
                 ->setAdmin(auth()->user()->id)
-                ->setData(
-                    ["Bulk Notification" =>  $notification->refresh()->toArray()],
-                )
+                ->setData([
+                    "Bulk Notification" => $notification->refresh()->toArray(),
+                ])
                 ->setUrl(request()->fullUrl())
                 ->log();
         }
@@ -110,20 +108,20 @@ class NotificationService
         if ($sendAt?->isPast() || is_null($sendAt)) {
             MethodsHelper::dispatchJob(new SendUserNotificationJob($notification));
             $notification->update([
-                "status" => StatusConstants::SENT
+                "status" => StatusConstants::SENT,
             ]);
 
             (new ActivityLogService)
                 ->setEvent("sent")
-                ->setTitle("Sent Bulk Notification")
+                ->setTitle("Bulk Notification Sent")
                 ->setDescription(auth()->user()?->full_name . " sent a bulk notification")
                 ->setType(ActivityLogConstants::SYSTEM_URL_TYPE)
                 ->setActivity(ActivitiesConstants::SENT_USER_NOTIFICATION)
                 ->setModel(SendBulkNotification::class, $notification->id)
                 ->setAdmin(auth()->user()->id)
-                ->setData(
-                    ["Bulk Notification" =>  $notification->refresh()->toArray()],
-                )
+                ->setData([
+                    "Bulk Notification" => $notification->toArray(),
+                ])
                 ->setUrl(request()->fullUrl())
                 ->log();
         }
@@ -131,21 +129,20 @@ class NotificationService
         return $notification;
     }
 
-
     public static function list(array $data = [])
     {
         $builder = SendBulkNotification::latest();
 
         if (!empty($type = $data["type"] ?? null)) {
-            $builder = $builder->where("type", $type);
+            $builder->where("type", $type);
         }
 
         if (!empty($status = $data["status"] ?? null)) {
-            $builder = $builder->where("status", $status);
+            $builder->where("status", $status);
         }
 
         if (!empty($recipients = $data["recipients"] ?? null)) {
-            $builder = $builder->whereHas('recipients', function ($query) use ($recipients) {
+            $builder->whereHas('recipients', function ($query) use ($recipients) {
                 $query->whereIn('user_id', $recipients);
             });
         }
@@ -155,17 +152,12 @@ class NotificationService
 
     public function delete(string $id)
     {
-        // Get the notification and store the old data before deletion
         $notification = self::getById($id);
         $old_notification_data = $notification->toArray();
-    
-        // Remove all recipients associated with this notification
-        $notification->recipients()->detach(); 
-    
-        // Delete the notification
+
+        $notification->recipients()->detach();
         $notification->delete();
-    
-        // Log the activity with the old data before the deletion
+
         (new ActivityLogService)
             ->setEvent("deleted")
             ->setTitle("Bulk Notification Deleted")
@@ -175,14 +167,13 @@ class NotificationService
             ->setModel(SendBulkNotification::class, $notification->id)
             ->setAdmin(auth()->user()->id)
             ->setData([
-                "Bulk Notification" => $old_notification_data, 
+                "Bulk Notification" => $old_notification_data,
             ])
             ->setUrl(request()->fullUrl())
             ->log();
-    
+
         return $notification;
     }
-    
 
     public function changeStatus(Request $request, $id)
     {
@@ -191,7 +182,7 @@ class NotificationService
             throw new InvalidRequestException("Invalid status provided");
         }
 
-        $notification = $this->getById($id);
+        $notification = self::getById($id);
         $notification->update([
             "status" => $status
         ]);
