e956f3b47fb69e3b675775945d59b126f9b92aab
Joel-prodevs <joel@prodevs.io>
2024-09-09 19:34:25 +0100

    Notifications update

--- config/app.php
+++ config/app.php
@@ -56,8 +56,8 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    
-    'web_url' => env('WEB_URL', 'https://web.talkam.prodevs.io'),
+
+    'web_url' => env('WEB_URL', 'https://web.talkam.prodevs.io/home/new'),
 
     'asset_url' => env('ASSET_URL'),
 
