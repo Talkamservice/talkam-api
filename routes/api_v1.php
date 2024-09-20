624d20c0dcb21e17000dd3f6f40ef719673228e3
Joel-prodevs <joel@prodevs.io>
2024-09-20 16:26:05 +0100

    Get by username endpoitn

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -68,6 +68,7 @@ Route::get("profile/avatars", [UserController::class, "listAvatars"])->name("ava
 Route::middleware(["auth:sanctum"])->group(function () {
     Route::prefix("user")->as("user.")->group(function () {
         Route::get("/me", [UserController::class, "me"])->name("me");
+        Route::get("/get-by-username", [UserController::class, "getByUsername"])->name("get-by-username");
 
         Route::prefix("profile")->as("profile.")->group(function () {
             Route::post("/upload-avatar", [UserController::class, "uploadAvatar"])->name("upload.avatar");
@@ -218,6 +219,7 @@ Route::prefix('user')->as('user.')->group(function () {
         Route::get("trending", [SearchController::class, "trending"])->name("trending");
         Route::get("suggestions", [SearchController::class, "suggestions"])->name("suggestions");
         Route::delete("{id}/delete", [SearchController::class, "destroy"])->name("destroy");
+        Route::get("/username", [UserController::class, "search"])->name("search");
     });
 
     Route::prefix("announcements")->as("announcements.")->group(function () {
