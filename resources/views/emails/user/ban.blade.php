01243e7deb00d4f0416c898925e212154b72277a
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-09-25 15:24:21 +0100

    minor changes

--- resources/views/emails/user/ban.blade.php
+++ resources/views/emails/user/ban.blade.php
@@ -7,14 +7,13 @@
         <p class="detailCont-p">
             {{ $message }}
         </p>
-        <p class="detailCont-ps">
-            f you believe this ban was issued in error or you have taken steps to address the violation, you may appeal this decision by submitting a request to our support team. [<span><a class="link" href="{{ config("app.web_url") . "/help&info/feedback" }}">Appeal here</a></span>].
-            To appeal, visit .
+        <p class="detailCont-p">
+            If you believe this ban was issued in error or you have taken steps to address the violation, you may appeal this decision by submitting a request to our support team. [<span><a class="link" href="{{ config("app.web_url") . "/help&info/feedback" }}">Appeal here</a></span>].
         </p>
-        <p class="detailCont-ps">
+        <p class="detailCont-p">
             Please note that appeals are reviewed carefully, and not all appeals will be granted.
         </p>
-        <p class="detailCont-ps">
+        <p class="detailCont-p">
             Thank you for your understanding.
         </p>
         <p class="detailCont-p">
