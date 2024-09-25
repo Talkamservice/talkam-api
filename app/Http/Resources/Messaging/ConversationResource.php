bec0d8fb789698ab2b5d933b4cda9a645f7ec356
Joel-prodevs <joel@prodevs.io>
2024-09-25 15:47:03 +0100

    Minor fix

--- app/Http/Resources/Messaging/ConversationResource.php
+++ app/Http/Resources/Messaging/ConversationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Messaging;
 
+use App\Constants\General\StatusConstants;
 use App\Http\Resources\Users\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -25,6 +26,7 @@ class ConversationResource extends JsonResource
             "notification_status" => $this->notification_status,
             "is_anonymous" => $this->is_anonymous,
             "requested_by" => UserResource::custom($this->user),
+            "user_is_banned" => $other_member?->status == StatusConstants::BANNED,
             "user_blocked" => isBlocked(auth()->id(), $other_member?->user_id),
             "i_am_blocked" => isBlocked($other_member?->user_id, auth()->id()),
             "status" => $this->status
