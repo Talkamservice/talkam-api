adf1f2f32a0a1723c679b7e0e312d08a441c05ed
Joel-prodevs <joel@prodevs.io>
2024-10-04 16:07:35 +0100

    Notifications update

--- app/Notifications/Bulk/SendBulkUsersNotification.php
+++ app/Notifications/Bulk/SendBulkUsersNotification.php
@@ -5,7 +5,8 @@ namespace App\Notifications\Bulk;
 use App\Services\Notifications\FirebaseNotificationService;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage; use App\Helpers\MethodsHelper;
+use Illuminate\Notifications\Messages\MailMessage;
+use App\Helpers\MethodsHelper;
 use Illuminate\Notifications\Notification;
 
 class SendBulkUsersNotification extends Notification implements ShouldQueue
@@ -15,10 +16,7 @@ class SendBulkUsersNotification extends Notification implements ShouldQueue
     /**
      * Create a new notification instance.
      */
-    public function __construct(public $user, public $notification)
-    {
-
-    }
+    public function __construct(public $user, public $notification) {}
 
     /**
      * Get the notification's delivery channels.
@@ -71,6 +69,9 @@ class SendBulkUsersNotification extends Notification implements ShouldQueue
             ->setBody($data["message"])
             ->setType($data["type"])
             ->byUserToken($notifiable->fcm_token)
+            ->setMetadata([
+                "extra" => []
+            ])
             ->initiate();
     }
 
