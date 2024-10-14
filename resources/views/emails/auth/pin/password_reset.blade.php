c018d7b5a6177be52f0cbb1bf2bd043193e7e859
Joel-prodevs <joel@prodevs.io>
2024-10-14 08:52:21 +0100

    Update the password mails

--- resources/views/emails/auth/pin/password_reset.blade.php
+++ resources/views/emails/auth/pin/password_reset.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="">
-        <p>Hi,</p>
+        <p>Hello {{ $user->getName() }},</p>
         <p class="detailCont-p">
-            Help us secure your account as you verify it is authentic.
+            You requested for a password reset on your account.
         </p>
 
         <p class="detailCont-p">
@@ -18,5 +18,6 @@
         <p class="detailCont-p">
             Regards, <br>TalkAM Team
         </p>
+        </td>
     </div>
 @endsection
