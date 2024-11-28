24c318b84acb33e18131cd6d6fb693df44a437a2
Joel-prodevs <joel@prodevs.io>
2024-11-28 09:21:06 +0100

    Fixed notification isue

--- app/Services/Finance/PaymentGateways/Flutterwave/FlutterwaveSubscriptionPaymentWebhookService.php
+++ app/Services/Finance/PaymentGateways/Flutterwave/FlutterwaveSubscriptionPaymentWebhookService.php
@@ -100,11 +100,11 @@ class FlutterwaveSubscriptionPaymentWebhookService
 
         $subscription = SubscriptionService::subscribeToPlan($this->user, $plan_duration);
 
-        // if ($this->user->should_display_ads != 1) {
+        if ($this->user->should_display_ads != 1) {
             $this->user?->update([
                 "should_display_ads" => 0
             ]);
-        // }
+        }
 
         Notification::send($this->user, new NewSubscriptionNotification($subscription));
         // Notification::send(sudo(), new AdminNewSubscriptionNotification($subscription));
