6c787a045b079ad3229a321ba944d5febeca94be
Joel-prodevs <joel@prodevs.io>
2024-11-28 08:57:04 +0100

    Disable ads for promotion

--- app/Services/Finance/PaymentGateways/Flutterwave/FlutterwaveOneOffPaymentWebhookService.php
+++ app/Services/Finance/PaymentGateways/Flutterwave/FlutterwaveOneOffPaymentWebhookService.php
@@ -146,9 +146,9 @@ class FlutterwaveOneOffPaymentWebhookService
                 throw new InvalidRequestException("We could not verify your promotion request.");
             }
 
-            // if (in_array($this->payment->status, [StatusConstants::FAILED, StatusConstants::COMPLETED])) {
-            //     throw new InvalidRequestException("Payment has already been verified.");
-            // }
+            if (in_array($this->payment->status, [StatusConstants::FAILED, StatusConstants::COMPLETED])) {
+                throw new InvalidRequestException("Payment has already been verified.");
+            }
 
             $this->payment->update([
                 "status" => StatusConstants::COMPLETED
@@ -157,6 +157,10 @@ class FlutterwaveOneOffPaymentWebhookService
             $promotion->update([
                 "status" => StatusConstants::ACTIVE,
                 "payment_id" => $this->payment->id,
+            ]);
+
+            $this->user?->update([
+                "should_display_ads" => 0
             ]);
 
             Notification::send($this->user, new NewPaymentNotification($this->payment));
