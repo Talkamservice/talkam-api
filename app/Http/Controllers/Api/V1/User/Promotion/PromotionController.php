a0ae9a40a71c4e5d77572948bf74f97a6e4769fc
Joel-prodevs <joel@prodevs.io>
2024-12-09 19:17:38 +0100

    Minor fix

--- app/Http/Controllers/Api/V1/User/Promotion/PromotionController.php
+++ app/Http/Controllers/Api/V1/User/Promotion/PromotionController.php
@@ -37,7 +37,7 @@ class PromotionController extends Controller
                 ->appends($request->query());
             $data = collectPagination($promotionResource);
 
-            $data["data"] = $data["data"]->map(function ($item) {
+            $data["data"] = $data["data"]?->map(function ($item) {
                 return new PromotionResource($item, false);
             });
             return ApiHelper::validResponse("Promotions returned successfully", $data);
