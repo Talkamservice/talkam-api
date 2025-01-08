215d71fca74646eff052b892e4d172dc45b6f833
Joelomojefe <joelomojefe@gmail.com>
2025-01-08 18:54:46 +0100

    Refactor engagement calculation methods to handle non-array reaction stats and ensure safe access to array elements

--- app/Services/Promotion/PromotionPricingService.php
+++ app/Services/Promotion/PromotionPricingService.php
@@ -97,7 +97,7 @@ class PromotionPricingService
                 throw new InvalidRequestException("The entered impressions can not be greater that the Average performance per day. Currently you have " . $avg_impressions_per_day);
             }
 
-            $data["max_daily_amount"] = $data['amount'] * 5;
+            $data["max_daily_amount"] = $data['amount'] * 10;
 
             $promotion_pricing = $this->getById($id);
             $promotion_pricing->update($data);
