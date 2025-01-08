ab6f1292cb9defa734d80fbe2ecfb82f24e39363
Joelomojefe <joelomojefe@gmail.com>
2025-01-08 18:09:40 +0100

    Refactor promotion statistics display to utilize PostStatsResource for improved data handling

--- app/Http/Resources/Stat/PostStatsResource.php
+++ app/Http/Resources/Stat/PostStatsResource.php
@@ -119,7 +119,7 @@ class PostStatsResource extends JsonResource
 
     public function calcEngagement($reaction_stats, $shares)
     {
-        $total_engagements = ($reaction_stats["comments"] + $reaction_stats["likes"] + $reaction_stats["dislikes"] + $shares);
+        $total_engagements = ($reaction_stats["comments"] ?? 0 + $reaction_stats["likes"] ?? 0 + $reaction_stats["dislikes"] ?? 0 + $shares);
         return $total_engagements;
     }
 
