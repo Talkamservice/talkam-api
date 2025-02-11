4bd4071806fc8bc8cceea9cfe25faa096bee8a2e
Joelomojefe <joelomojefe@gmail.com>
2025-02-11 10:39:26 +0100

    Minor fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -50,6 +50,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('promotion:send-expired-notifications')->everyMinute();
         $schedule->command('finance:currency_rates')->weekly();
         $schedule->command('process:post-stats-command')->everyThreeHours();
+        $schedule->command('telescope:prune')->everySixHours();
     }
 
     /**
